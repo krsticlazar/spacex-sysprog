02c99162dc51373aa91b89655efa4f320970efb8
krsticlazar <krsticlazar@elfak.rs>
2025-09-11 23:15:36 +0200

    Skelet

---
+++ spacex-sysprog/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace spacex_sysprog;
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+
+    }
+
+    private static async Task<AppSettings> LoadSettingsAsync()
+    {
+
+    }
+}
